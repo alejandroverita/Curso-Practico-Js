10520634353a4bf98ba2f5f769362d7bc2042716
avera6756 <avera6756@utm.edu.ec>
2021-06-27 19:44:31 -0500

    interactuando con js desde html

--- Figures/figures.js
+++ Figures/figures.js
@@ -41,3 +41,60 @@ const areaCircle = (radio) => radio * radio * PI;
 
 
 console.groupEnd();
+
+// Interation with html
+
+/* SQUARE */
+
+function calcularPerimetroCuadrado(){
+    const input = document.getElementById("inputCuadrado");
+
+    const value = input.value; //obtenemos el valor del input
+
+    const perimeter = perimeterSquare(value);
+
+    alert(perimeter);
+}
+
+function calcularAreaCuadrado(){
+    const input = document.getElementById("inputCuadrado");
+
+    const value = input.value; //obtenemos el valor del input
+
+    const area = areaSquare(value);
+
+    alert(area);
+}
+
+/* TRIANGLE */
+
+function calcularPerimetroTriangle(){
+    const lado1 = parseInt(document.getElementById("inputSide1").value);
+    const lado2 = parseInt(document.getElementById("inputSide2").value);
+    const base = parseInt(document.getElementById("inputBase").value);
+
+    alert(`El perimetro del triangulo es: ${lado1+lado2+base} cm`);
+}
+
+function calcularAreaTriangle(){
+    const height = parseInt(document.getElementById("inputHeight").value);
+
+    const base = parseInt(document.getElementById("inputBase").value);
+
+    alert(`El area del triangulo es ${areaTriangle(base, height)} cms^2`);
+}
+
+
+/* CIRCLE */
+
+function calcularPerimetroCircle(){
+    const radio = parseInt(document.getElementById("radio").value);
+
+    alert(`El perimetro del circulo es ${perimeterCircle(radio)} cms`);
+}
+
+function calcularAreaCircle(){
+    const radio = parseInt(document.getElementById("radio").value);
+
+    alert(`El area del circulo es ${areaCircle(radio)} cms^2`);
+}
